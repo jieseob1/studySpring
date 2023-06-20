f31575f5cdd3b9afd38ce264e6f0cfdb474a5ff4
jiseob park <jieseob1@gmail.com>
2023-06-20 16:06:25 +0900

    Download Spring initializer and apply to basic folder

---
+++ basic/src/test/java/com/basic/springbasic/SpringbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.springbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
